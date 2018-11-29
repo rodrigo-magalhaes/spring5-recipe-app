8f8636f877b9e5cc26c8b24e9440cedcd9606dbe
rodrigouau <rodrigouau@yahoo.com.br>
2018-11-29 19:33:33 -0200

    added classes and relationships between them.

---
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -0,0 +1,58 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal amout;
+    @ManyToMany
+    private Recipe recipe;
+    @OneToOne
+    private UnitOfMeasure unitOfMeasure;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmout() {
+        return amout;
+    }
+
+    public void setAmout(BigDecimal amout) {
+        this.amout = amout;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
+    }
+}
