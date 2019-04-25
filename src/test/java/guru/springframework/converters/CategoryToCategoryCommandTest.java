0185a521fd8206bb1ccf11d8b0ea375148525d2a
rodrigo.magalhaes <rodrigo.magalhaes@projuris.com.br>
2019-04-25 07:04:08 -0300

    added commands, converters, new service methods and tests

---
+++ src/test/java/guru/springframework/converters/CategoryToCategoryCommandTest.java
@@ -0,0 +1,47 @@
+package guru.springframework.converters;
+
+
+import guru.springframework.commands.CategoryCommand;
+import guru.springframework.domain.Category;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CategoryToCategoryCommandTest {
+
+    public static final Long ID_VALUE = new Long(1L);
+    public static final String DESCRIPTION = "descript";
+    CategoryToCategoryCommand converter;
+
+    @Before
+    public void setUp() {
+        converter = new CategoryToCategoryCommand();
+    }
+
+    @Test
+    public void testNullObject() {
+        assertNull(converter.convert(null));
+    }
+
+    @Test
+    public void testEmptyObject() {
+        assertNotNull(converter.convert(new Category()));
+    }
+
+    @Test
+    public void convert() {
+        //given
+        Category category = new Category();
+        category.setId(ID_VALUE);
+        category.setDescription(DESCRIPTION);
+
+        //when
+        CategoryCommand categoryCommand = converter.convert(category);
+
+        //then
+        assertEquals(ID_VALUE, categoryCommand.getId());
+        assertEquals(DESCRIPTION, categoryCommand.getDescription());
+
+    }
+}
